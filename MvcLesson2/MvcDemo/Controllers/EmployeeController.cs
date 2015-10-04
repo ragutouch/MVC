16c66e5dd0cc4468a2616cab539d92e715a582ee
RagunathaBoopathy <ragutouch@gmail.com>
2015-10-05 01:55:43 +0530

    Mvc lesson2

---
+++ MvcLesson2/MvcDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDemo.Models;
+
+namespace MvcDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeContext db = new EmployeeContext();
+
+        public ActionResult Index()
+        {
+            var employees = db.Employees.Include(e => e.Department);
+            return View(employees.ToList());
+        }
+
+        public ActionResult EmployeesByDepartment()
+        {
+            var employees = db.Employees.Include(e => e.Department)
+                                .GroupBy(x => x.Department.Name)
+                                .Select(y => new DepartmentTotal
+                                    {
+                                        Name = y.Key,
+                                        Total = y.Count()
+                                    }).ToList().OrderByDescending(y => y.Total);
+            return View(employees);
+        }
+
+        //
+        // GET: /Employee/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        //
+        // GET: /Employee/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.departmentID = new SelectList(db.Departments, "id", "Name");
+            return View();
+        }
+
+        //
+        // POST: /Employee/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.departmentID = new SelectList(db.Departments, "id", "Name", employee.departmentID);
+            return View(employee);
+        }
+
+        //
+        // GET: /Employee/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.departmentID = new SelectList(db.Departments, "id", "Name", employee.departmentID);
+            return View(employee);
+        }
+
+        //
+        // POST: /Employee/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.departmentID = new SelectList(db.Departments, "id", "Name", employee.departmentID);
+            return View(employee);
+        }
+
+        //
+        // GET: /Employee/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        //
+        // POST: /Employee/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
